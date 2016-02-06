e47b448938579f2af69c6482a7a11f8b023205f7
Andrew Hill <mdude2314@gmail.com>
2016-02-06 17:27:47 -0500

    Render tutorials on screen. Not sortable yet.

---
+++ app/scripts/controllers/tutorial.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name homework4App.controller:TutorialCtrl
+ * @description
+ * # TutorialCtrl
+ * Controller of the homework4App
+ */
+angular.module('homework4App')
+  .controller('TutorialCtrl', function () {
+    
+    this.tutorials = tutorials;
+  });
